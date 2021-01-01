506b9195df68a2251a527ef1d9e0b51d958f631c
Joshua Coronel <Joshuajonme@gmail.com>
2020-12-31 22:58:38 -0500

    react web design

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Switch} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+
+import './App.css'
+
+function App() {
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route path='/upload' component={Upload}/>
+        <Route path='/' component={Home}/>
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +27,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
